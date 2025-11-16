71b737926c2cc15dacde4c81f13a2ae2b2193cd0
Brian-Conley <brianjconleyjr@gmail.com>
2025-11-16 15:13:44 -0500

    Initial commit

---
+++ game.js
@@ -0,0 +1,173 @@
+"use strict";
+
+function makeRectangularPrism(center, dimensions, rotation, color) {
+
+    var theta = [0, 0, 0];
+    var translation = [0 ,0 ,0];
+
+    var vertexColors = [
+        color,
+        color,
+        color,
+        color,
+        color,
+        color,
+        color,
+        color
+    ];
+
+    const lengthh = dimensions[0] / 2;
+    const heighth = dimensions[1] / 2;
+    const widthh = dimensions[2] / 2;
+
+    var local_vertices = [
+        vec3( -lengthh, -heighth,  widthh ),
+        vec3( -lengthh,  heighth,  widthh ),
+        vec3(  lengthh,  heighth,  widthh ),
+        vec3(  lengthh, -heighth,  widthh ),
+        vec3( -lengthh, -heighth, -widthh ),
+        vec3( -lengthh,  heighth, -widthh ),
+        vec3(  lengthh,  heighth, -widthh ),
+        vec3(  lengthh, -heighth, -widthh )
+    ];
+    var vertices = [];
+
+    var rotx = rotateX(rotation[0]);
+    var roty = rotateY(rotation[1]);
+    var rotz = rotateZ(rotation[2]);
+    var rot = mult(rotx, mult(roty, rotz));
+    var t = mult(translate(center), rot);
+
+    for (let v of local_vertices) {
+        var v4 = vec4(v, 1);
+        var rot_v4 = vec3( dot(v4, t[0]), dot(v4, t[1]), dot(v4, t[2]) );
+        vertices.push(vec3( rot_v4[0], rot_v4[1], rot_v4[2] ));
+    }
+
+    var indices = [
+        1, 0, 3,
+        3, 2, 1,
+        2, 3, 7,
+        7, 6, 2,
+        3, 0, 4,
+        4, 7, 3,
+        6, 5, 1,
+        1, 2, 6,
+        4, 5, 6,
+        6, 7, 4,
+        5, 4, 0,
+        0, 1, 5
+    ];
+
+    // array element buffer
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    // color array atrribute buffer
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
+
+    // vertex array attribute buffer
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var boundingBox = {
+        min: vec3( center[0] - lengthh, center[1] - heighth, center[2] - widthh ),
+        max: vec3( center[0] + lengthh, center[1] + heighth, center[2] + widthh ),
+    }
+
+    return {
+        iBuffer: iBuffer,
+        cBuffer: cBuffer,
+        vBuffer: vBuffer,
+        translation: translation,
+        theta: theta,
+        vertices: vertices,
+        vertexColors: vertexColors,
+        numVertices: 36,
+        indices: indices,
+        boundingBox: boundingBox,
+        AABB: function() {
+            let bbm = this.boundingBox.min;
+            let bbM = this.boundingBox.max;
+            let t = this.translation;
+            return {
+                min: vec3( bbm[0] + t[0], bbm[1] + t[1], bbm[2] + t[2] ),
+                max: vec3( bbM[0] + t[0], bbM[1] + t[1], bbM[2] + t[2] )
+            }
+        }
+    }
+
+}
+
+var canvas;
+var gl;
+
+var thetaLoc;
+var translationLoc;
+var vPosition;
+var vColor;
+
+var shapes;
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);;
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    vColor = gl.getAttribLocation( program, "vColor" );
+    vPosition = gl.getAttribLocation( program, "vPosition" );
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+    translationLoc = gl.getUniformLocation(program, "translation");
+
+    shapes = [
+        makeRectangularPrism(vec3(0,0,0), vec3(.1,.1,.1), vec3(0,0,0), vec4(1,0,0,1)),
+        makeRectangularPrism(vec3(-.25,0,0), vec3(.1,.1,.1), vec3(0,0,0), vec4(0,1,0,1)),
+        makeRectangularPrism(vec3(.25,0,0), vec3(.1,.1,.1), vec3(0,0,0), vec4(0,0,1,1))
+    ];
+
+    render();
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+
+    for (var shape of shapes) {
+
+        shape.theta[1] += 2.0;
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, shape.iBuffer);
+
+        gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vPosition );
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, shape.cBuffer );
+
+        gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vColor );
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, shape.vBuffer );
+
+        gl.uniform3fv(thetaLoc, shape.theta);
+        gl.uniform3fv(translationLoc, shape.translation);
+        gl.drawElements( gl.TRIANGLES, shape.numVertices, gl.UNSIGNED_BYTE, 0 );
+    }
+
+    requestAnimFrame( render );
+}
